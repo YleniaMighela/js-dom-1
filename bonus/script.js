750fa931c20ec3ed2a0b13b4c4b62a4a948030cd
Ylenia <yleniamighela@gmail.com>
2025-01-11 15:57:18 +0100

    [fix] add exercise bonus

--- bonus/script.js
+++ bonus/script.js
@@ -1,23 +1,42 @@
-// Inseriamo in pagina l’immagine della lampadina spenta che trovate in allegato e accanto un bottone con la scritta “Accendi”.
-// Al click del bottone, la lampadina dovrà accendersi (dovremo quindi utilizzare l’immagine della lampadina accesa, sempre in allegato)
-
+// BONUS
+// Facciamo accendi e spegni:
+// Al primo click la lampadina si accende e nel bottone compare la scritta “Spegni”
+// Al secondo click la lampadina si spegne e nel bottone compare la scritta “Accendi”
+// E così via...
 
 
 // seleziono gli elementi da richiamare nel click
 
+// elemento del bottone
 const bottone = document.querySelector (".pulsante");
 console.log(bottone);
 
+
+// elemento dell'immagine
 const lampadinaGialla = document.getElementById ('lampadinabianca');
 console.log(lampadinaGialla);
 
 
-// creo l'evento del click
 
+// creo l'evento del click 
 bottone.addEventListener ('click', function(){
-   
-    lampadinaGialla.src = "./img/yellow_lamp.png";
-
-    console.log(lampadinaGialla);
+    
+    // creo una variabile per capire se la classe bottone include un contenuto "ACCENDI"
+    const press = bottone.innerHTML.includes("ACCENDI");
+    
+    // se è vero accende la lampadina e restituisce il testo "SPEGNI"
+    if (press){
+        
+        lampadinaGialla.src = "./img/yellow_lamp.png";
+        bottone.innerHTML = "SPEGNI";
+        
+        //    altrimenti la lampadina di spegne e restituisce il testo "ACCENDI"
+    } else  {
+        
+        lampadinaGialla.src = "img/white_lamp.png";
+        bottone.innerHTML = "ACCENDI";
+        
+    }
+    
     
 });
